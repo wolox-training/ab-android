477743d4f88bb46fb5f88197a23651629277e273
alejandrobodichon <alejandro.bodichon@gmail.com>
2019-02-04 15:00:59 -0300

    add components of login (#1)
    * first commit
    
    * add components of login
    
    * text from spanish to english
    
    * change constants to resources files(PR)
    
    * changes of PR, colors to colors.xml file

--- app/src/main/java/ar/com/wolox/android/example/di/AppComponent.kt
+++ app/src/main/java/ar/com/wolox/android/example/di/AppComponent.kt
@@ -4,7 +4,6 @@ import android.app.Application
 
 import ar.com.wolox.android.example.BootstrapApplication
 import ar.com.wolox.android.example.di.module.MiscModule
-import ar.com.wolox.android.example.ui.viewpager.ViewPagerActivityModule
 import ar.com.wolox.wolmo.core.di.modules.ContextModule
 import ar.com.wolox.wolmo.core.di.modules.DefaultModule
 import ar.com.wolox.wolmo.core.di.scopes.ApplicationScope
@@ -18,7 +17,7 @@ import dagger.android.support.AndroidSupportInjectionModule
 @ApplicationScope
 @Component(dependencies = [NetworkingComponent::class],
         modules = [AndroidSupportInjectionModule::class, DefaultModule::class, ContextModule::class,
-            AppModule::class, ViewPagerActivityModule::class, MiscModule::class])
+            AppModule::class, MiscModule::class])
 interface AppComponent : AndroidInjector<BootstrapApplication> {
 
     @Component.Builder
